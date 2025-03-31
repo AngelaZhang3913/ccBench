1ce41358ce9320a9da5387fd4d061c14473db751
Jane Chen <janechen@cs.utexas.edu>
2025-03-31 11:40:15 -0500

    Remove tmp files changed more than 2 minutes ago

--- pantheon-modified/src/experiments/palantir_clean.sh
+++ pantheon-modified/src/experiments/palantir_clean.sh
@@ -1,7 +1,7 @@
 
 ls -trlh ../../tmp/ | awk '{print $7" "$8" "$9}' | sed "s/:/ /g" | awk '{a=($1*24+$2)*60+$3; print a" "$4}' > files
 now=`date | awk '{print $3" "$4}' | sed "s/:/ /g" | awk '{print ($1*24+$2)*60+$3}'`;
-cat files | awk -v now="$now" '{if($1<(now-20))print $2}' > remove-them
+cat files | awk -v now="$now" '{if($1<(now-2))print $2}' > remove-them
 for i in `cat remove-them`; do rm -f ../../tmp/$i;done
 rm files remove-them
 
