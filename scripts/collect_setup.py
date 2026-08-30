dd7de5ee5721cbb6392dba3ae640f35222ddc7d0
angelaz <angelaz@node0.angelaz-314020.ldos-ut-pg0.wisc.cloudlab.us>
2026-08-30 10:55:16 -0500

    change mahimahi_traces dir in collect_setup to match generation

--- scripts/collect_setup.py
+++ scripts/collect_setup.py
@@ -65,7 +65,7 @@ def run_remote_collection(server, schemes):
             # Start tmux session and generate the bandwidth traces for collection
             "tmux new-session -d -s collect",
             "tmux send-keys -t collect 'source ~/venv/bin/activate' C-m",  # Activate virtual environment
-            "tmux send-keys -t collect 'cd ~/ccBench/pantheon-modified/src/experiments' C-m",
+            "tmux send-keys -t collect 'cd ~/ccBench' C-m",
             "tmux send-keys -t collect 'python ~/ccBench/scripts/generate_mahimahi_traces.py' C-m",  # Set PYTHONPATH
         ]
 
