7046aa96d1b27a419b1716d91ece8f99249398a4
angelaz <angelaz@node0.angelaz-314785.ldos-ut-pg0.wisc.cloudlab.us>
2026-09-06 22:08:31 -0500

    remove pure from collect_setup

--- scripts/collect_setup.py
+++ scripts/collect_setup.py
@@ -19,7 +19,7 @@ servers = config["servers"]
 username = "angelaz"
 
 # List of congestion control schemes
-cc_schemes = ["reno", "pure", "cubic", "vegas", "bbr", "cdg", "hybla", "highspeed", "illinois",
+cc_schemes = ["reno", "cubic", "vegas", "bbr", "cdg", "hybla", "highspeed", "illinois",
               "westwood", "yeah", "htcp", "bic", "veno"]
 
 num_servers = len(servers)
